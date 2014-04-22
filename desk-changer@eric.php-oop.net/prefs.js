4710b241fc20176161fb3f14e363f7ea17ae03bc
Eric Gach <eric@php-oop.net>
2014-04-22 10:14:31 -0500

    updating prefs so the start of editing profiles is possible.

--- desk-changer@eric.php-oop.net/prefs.js
+++ desk-changer@eric.php-oop.net/prefs.js
@@ -1,3 +1,16 @@
+/**
+ *  ▄▄▄▄    ██▓  ▄████   ▄████  ██▓▓█████
+ * ▓█████▄ ▓██▒ ██▒ ▀█▒ ██▒ ▀█▒▓██▒▓█   ▀
+ * ▒██▒ ▄██▒██▒▒██░▄▄▄░▒██░▄▄▄░▒██▒▒███
+ * ▒██░█▀  ░██░░▓█  ██▓░▓█  ██▓░██░▒▓█  ▄
+ * ░▓█  ▀█▓░██░░▒▓███▀▒░▒▓███▀▒░██░░▒████▒
+ * ░▒▓███▀▒░▓   ░▒   ▒  ░▒   ▒ ░▓  ░░ ▒░ ░
+ * ▒░▒   ░  ▒ ░  ░   ░   ░   ░  ▒ ░ ░ ░  ░
+ *  ░    ░  ▒ ░░ ░   ░ ░ ░   ░  ▒ ░   ░
+ *  ░       ░        ░       ░  ░     ░  ░
+ *       ░
+ */
+
 const Gdk = imports.gi.Gdk;
 const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
@@ -27,14 +40,14 @@ const DeskChangerPrefs = new Lang.Class({
 
 	toggle_subfolders: function(widget, path)
 	{
-		var iter = this._profiles.get_iter_from_string(path)[1];
-		this._profiles.set_value(iter, 1, !this._profiles.get_value(iter, 1));
+		var iter = this._folders.get_iter_from_string(path)[1];
+		this._folders.set_value(iter, 1, !this._folders.get_value(iter, 1));
 	},
 
 	_initProfiles: function ()
 	{
-		this._profiles = new Gtk.ListStore();
-		this._profiles.set_column_types([GObject.TYPE_STRING, GObject.TYPE_BOOLEAN]);
+		this._folders = new Gtk.ListStore();
+		this._folders.set_column_types([GObject.TYPE_STRING, GObject.TYPE_BOOLEAN]);
 		var profiles_box = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL});
 		var hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL});
 		var label = new Gtk.Label({label: 'Profile'});
@@ -44,10 +57,10 @@ const DeskChangerPrefs = new Lang.Class({
 		this.profiles_combo_box.connect('changed', Lang.bind(this, function(object) {
 			for (var profile in this._settings.profiles) {
 				if (profile == object.get_active_text()) {
-					this._profiles.clear();
+					this._folders.clear();
 					for (var folder in this._settings.profiles[profile]) {
 						folder = [this._settings.profiles[profile][folder][0].replace('file://', ''), this._settings.profiles[profile][folder][1]];
-						this._profiles.insert_with_valuesv(-1, [0, 1], folder);
+						this._folders.insert_with_valuesv(-1, [0, 1], folder);
 					}
 				}
 			}
@@ -58,12 +71,16 @@ const DeskChangerPrefs = new Lang.Class({
 
 		this.profiles = new Gtk.TreeView();
 		this.profiles.connect('cursor_changed', Lang.bind(this, function (treeview) {
-			debug(treeview);
-			this.edit.set_sensitive(true);
 			this.remove.set_sensitive(true);
 		}));
-		this.profiles.set_model(this._profiles);
+		this.profiles.set_model(this._folders);
 		var renderer = new Gtk.CellRendererText();
+		renderer.set_property('editable', true);
+		renderer.connect('edited', Lang.bind(this, function (renderer, path, new_text) {
+			var [bool, iter] = this._folders.get_iter_from_string(path);
+			this._folders.set_value(iter, 0, new_text);
+			this._save_profile();
+		}));
 		var column = new Gtk.TreeViewColumn({title: 'Path', expand: true});
 		column.pack_start(renderer, true);
 		column.add_attribute(renderer, 'text', 0);
@@ -87,16 +104,30 @@ const DeskChangerPrefs = new Lang.Class({
 		this.profiles_combo_box.set_active(active);
 
 		hbox = new Gtk.ButtonBox({orientation: Gtk.Orientation.HORIZONTAL});
-		this.edit = new Gtk.Button({label: 'Edit'});
-		this.edit.set_sensitive(false);
-		hbox.pack_start(this.edit, false, false, 10);
+		this.add = new Gtk.Button({label: 'Add Profile'});
+		this.add.set_sensitive(true);
+		hbox.pack_start(this.add, true, true, 10);
 		this.remove = new Gtk.Button({label: 'Remove'});
 		this.remove.set_sensitive(false);
-		hbox.pack_start(this.remove, false, false, 10);
+		hbox.pack_end(this.remove, false, false, 10);
 		profiles_box.pack_end(hbox, true, true, 10);
 
 		profiles_box.pack_start(this.profiles, true, true, 10);
 		this.notebook.append_page(profiles_box, new Gtk.Label({label: 'Profiles'}));
+	},
+
+	_save_profile: function()
+	{
+		var profile = [];
+		this._folders.foreach(Lang.bind(profile, function (model, path, iter) {
+			this.push([model.get_value(iter, 0), model.get_value(iter, 1)]);
+		}));
+		debug(JSON.stringify(profile));
+		debug(this.profiles_combo_box.get_active_text());
+		var profiles = this._settings.profiles;
+		profiles[this.profiles_combo_box.get_active_text()] = profile;
+		this._settings.profiles = profiles;
+		this._initProfiles();
 	}
 });
 
