8f10a14da5d5c7e267d38e1e860e76d03ec9e5d4
Eric Gach <eric@php-oop.net>
2014-05-22 11:11:25 -0500

    version bump

--- desk-changer@eric.php-oop.net/daemon.py
+++ desk-changer@eric.php-oop.net/daemon.py
@@ -31,7 +31,7 @@ import traceback
 
 __author__ = 'Eric Gach <eric@php-oop.net>'
 __daemon_path__ = os.path.dirname(os.path.realpath(__file__))
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 _logger = logging.getLogger('desk-changer')
 
