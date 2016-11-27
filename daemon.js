0b73cf31c1227792000c5f3c56dba80f57f1af1b
Eric Gach <eric.gach@gmail.com>
2016-11-27 03:18:24 -0600

    Updating build process and rewriting daemon in python

--- daemon.js
+++ daemon.js
@@ -103,7 +103,7 @@ const DeskChangerDaemon = new Lang.Class({
             this.bus.QuitSync();
         } else {
             debug('starting daemon');
-            GLib.spawn_async(this._path, [this._path + '/desk-changer-daemon'], null, GLib.SpawnFlags.DO_NOT_REAP_CHILD, null);
+            GLib.spawn_async(this._path, [this._path + '/desk-changer-daemon.py'], null, GLib.SpawnFlags.DO_NOT_REAP_CHILD, null);
         }
     },
 
