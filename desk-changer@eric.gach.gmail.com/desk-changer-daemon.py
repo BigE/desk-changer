6f4db7ca98983c5d79f9f533cc9a148e40a9f29b
Eric Gach <eric.gach@gmail.com>
2017-04-24 21:50:00 -0500

    Fixing settings path

--- desk-changer@eric.gach.gmail.com/desk-changer-daemon.py
+++ desk-changer@eric.gach.gmail.com/desk-changer-daemon.py
@@ -5,9 +5,8 @@ import os.path
 import sys
 from deskchanger.application import Daemon
 
-__daemon_path__ = os.path.abspath(os.curdir)
-
 if __name__ == '__main__':
+    __daemon_path__ = os.path.abspath(os.path.dirname(__file__))
     os.environ['GSETTINGS_SCHEMA_DIR'] = os.path.join(__daemon_path__, 'schemas')
     daemon = Daemon()
     daemon.run(sys.argv)
