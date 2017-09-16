5e8b4bc609be0999b92aaab7b0cbc2d7d3b272ec
Eric Gach <eric.gach@gmail.com>
2017-09-15 23:01:28 -0500

    Bumping version for release

--- desk-changer@eric.gach.gmail.com/deskchanger/__init__.py
+++ desk-changer@eric.gach.gmail.com/deskchanger/__init__.py
@@ -4,4 +4,4 @@ import os.path
 gettext.bindtextdomain('desk-changer-daemon', os.path.join(os.path.dirname(__file__), '..', 'locale'))
 gettext.textdomain('desk-changer-daemon')
 _ = gettext.gettext
-__version__ = '2.4.0'
+__version__ = '2.4.1'
