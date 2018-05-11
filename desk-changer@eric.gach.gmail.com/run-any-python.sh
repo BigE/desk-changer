ea4d681dd2f0fef18b388f7afc6b53e8aad26b86
Dmytro Cherepovskyi <codernumber1@gmail.com>
2018-05-12 01:24:13 +0200

    use bash wrapper to choose which python runs the daemon

---
+++ desk-changer@eric.gach.gmail.com/run-any-python.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+CODE_WRONG_USAGE=255
+CODE_NO_PYTHON=254
+
+# input params
+
+if [ $# -eq 0 ]
+  then
+  	echo "Runs the given script with the first found Python version"
+    echo "Usage: $0 <PYTHON_SCRIPT>"
+    exit $CODE_WRONG_USAGE
+fi
+SCRIPT=$1
+
+# python version checker
+
+PYTHON2='/usr/bin/env python'
+PYTHON3='/usr/bin/env python3'
+
+function check_python() {
+	eval "$1 -V &> /dev/null"
+	if [ $? -eq 0 ]; then
+		true
+	else
+		false
+	fi;
+}
+
+if check_python "$PYTHON2"; then
+	eval "$PYTHON2 \"$SCRIPT\""
+elif check_python "$PYTHON3"; then
+	eval "$PYTHON3 \"$SCRIPT\""
+else
+	echo "NO PYTHON DETECTED!"
+	exit $CODE_NO_PYTHON
+fi;
