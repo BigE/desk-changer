29f75eca98fd207a2f7b21e03b42329270d4e967
Dmytro Cherepovskyi <codernumber1@gmail.com>
2018-05-12 12:05:02 +0200

    convert bash script to spaces

--- desk-changer@eric.gach.gmail.com/run-any-python.sh
+++ desk-changer@eric.gach.gmail.com/run-any-python.sh
@@ -7,7 +7,7 @@ CODE_NO_PYTHON=254
 
 if [ $# -eq 0 ]
   then
-  	echo "Runs the given script with the first found Python version"
+    echo "Runs the given script with the first found Python version"
     echo "Usage: $0 <PYTHON_SCRIPT>"
     exit $CODE_WRONG_USAGE
 fi
@@ -19,19 +19,19 @@ PYTHON2='/usr/bin/env python'
 PYTHON3='/usr/bin/env python3'
 
 function check_python() {
-	eval "$1 -V &> /dev/null"
-	if [ $? -eq 0 ]; then
-		true
-	else
-		false
-	fi;
+    eval "$1 -V &> /dev/null"
+    if [ $? -eq 0 ]; then
+        true
+    else
+        false
+    fi;
 }
 
 if check_python "$PYTHON2"; then
-	eval "$PYTHON2 \"$SCRIPT\""
+    eval "$PYTHON2 \"$SCRIPT\""
 elif check_python "$PYTHON3"; then
-	eval "$PYTHON3 \"$SCRIPT\""
+    eval "$PYTHON3 \"$SCRIPT\""
 else
-	echo "NO PYTHON DETECTED!"
-	exit $CODE_NO_PYTHON
+    echo "NO PYTHON DETECTED!"
+    exit $CODE_NO_PYTHON
 fi;
