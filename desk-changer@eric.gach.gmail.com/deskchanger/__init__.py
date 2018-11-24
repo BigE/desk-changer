f8ab9d640e75f070e376b6190b1dfaa91917e263
Eric Gach <eric.gach@gmail.com>
2018-11-24 00:08:31 -0600

    Removing python

--- desk-changer@eric.gach.gmail.com/deskchanger/__init__.py
+++
@@ -1,7 +0,0 @@
-import gettext
-import os.path
-
-gettext.bindtextdomain('desk-changer-daemon', os.path.join(os.path.dirname(__file__), '..', 'locale'))
-gettext.textdomain('desk-changer-daemon')
-_ = gettext.gettext
-__version__ = '2.4.1'
