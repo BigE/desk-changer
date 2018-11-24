9fadd2c2e9593be122552457d204bc7f1422106b
Eric Gach <eric.gach@gmail.com>
2018-11-24 00:08:55 -0600

    Updating copyright info

--- desk-changer@eric.gach.gmail.com/extension.js
+++ desk-changer@eric.gach.gmail.com/extension.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2017 Eric Gach <eric.gach@gmail.com>
+ * Copyright (c) 2014-2018 Eric Gach <eric.gach@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- desk-changer@eric.gach.gmail.com/prefs.js
+++ desk-changer@eric.gach.gmail.com/prefs.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2015 Eric Gach <eric@php-oop.net>
+ * Copyright (c) 2014-2018 Eric Gach <eric.gach@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
