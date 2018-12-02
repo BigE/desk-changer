c08acf4fa8405ea0e565725cbe8956b5c6e7d628
Eric Gach <eric.gach@gmail.com>
2018-12-01 23:46:31 -0600

    Updating headers

--- desk-changer@eric.gach.gmail.com/daemon.js
+++ desk-changer@eric.gach.gmail.com/daemon.js
@@ -19,6 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 const Lang = imports.lang;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const GLib = imports.gi.GLib;
