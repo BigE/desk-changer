c048be0b6ee6141c975c008a64d040cb7ff152f7
Eric Gach <eric.gach@gmail.com>
2019-10-19 18:33:31 -0500

    Updating how settings work

--- desk-changer@eric.gach.gmail.com/convenience.js
+++ desk-changer@eric.gach.gmail.com/convenience.js
@@ -27,6 +27,7 @@
 
 const Gettext = imports.gettext;
 const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
 
 const Config = imports.misc.config;
 const ExtensionUtils = imports.misc.extensionUtils;
@@ -87,8 +88,187 @@ function getSettings(schema) {
 
     let schemaObj = schemaSource.lookup(schema, true);
     if (!schemaObj) {
-        throw new Error('Schema ' + schema + ' could not be found for extension ' + extension.metadata.uuid + '. Please check your installation.');
+        throw new Error(`Schema ${schema} could not be found for extension ${extension.metadata.uuid}. Please check your installation.`);
     }
 
-    return new Gio.Settings({settings_schema: schemaObj});
+    return new Settings({settings_schema: schemaObj});
+}
+
+function debug(message, caller) {
+    let extension = ExtensionUtils.getCurrentExtension(),
+        output = `[${extension.metadata.uuid}/${caller.split('/').pop()}] ${message}`;
+
+    log(output);
+}
+
+let Settings = GObject.registerClass(
+class DeskChangerSettings extends Gio.Settings {
+    get auto_start() {
+        return this.get_boolean('auto-start');
+    }
+
+    set auto_start(value) {
+        value = Boolean(value);
+        this.set_boolean('auto-start', value);
+
+        if (this.debug) {
+            let caller = getCaller();
+            debug(`set auto-start: ${value}`, getCaller());
+        }
+    }
+
+    get current_profile() {
+        return this.get_string('current-profile');
+    }
+
+    set current_profile(value) {
+        this.set_string('current-profile', value);
+
+        if (this.debug) {
+            debug(`set current-profile: ${value}`, getCaller());
+        }
+    }
+
+    get debug() {
+        return this.get_boolean('debug');
+    }
+
+    set debug(value) {
+        this.set_boolean('debug', Boolean(value));
+        debug(`set debug: ${value}`, getCaller());
+    }
+
+    get icon_preview() {
+        return this.get_boolean('icon-preview');
+    }
+
+    set icon_preview(value) {
+        this.set_boolean('icon-preview', Boolean(value));
+
+        if (this.debug) {
+            debug(`set icon-preview: ${value}`, getCaller());
+        }
+    }
+
+    get interval() {
+        return this.get_int('interval');
+    }
+
+    set interval(value) {
+        this.set_int('interval', value);
+
+        if (this.debug) {
+            debug(`set interval: ${value}`, getCaller());
+        }
+    }
+
+    get profiles() {
+        return this.get_value('profiles').deep_unpack();
+    }
+
+    set profiles(value) {
+        this.set_value('profiles', new GLib.Variant('a{sa(sb)}', value));
+
+        if (this.debug) {
+            debug(`set profiles: ${value}`, getCaller());
+        }
+    }
+
+    get notifications() {
+        return this.get_boolean('notifications');
+    }
+
+    set notifications(value) {
+        value = Boolean(value);
+        this.set_boolean('notifications', value);
+
+        if (this.debug) {
+            debug(`set notifications: ${value}`, getCaller());
+        }
+    }
+
+    get random() {
+        return this.get_boolean('random');
+    }
+
+    set random(value) {
+        value = Boolean(value);
+        this.set_boolean('random', value);
+
+        if (this.debug) {
+            debug(`set random: ${value}`, getCaller());
+        }
+    }
+
+    get rotation() {
+        return this.get_string('rotation');
+    }
+
+    set rotation(value) {
+        this.set_string('rotation', value);
+
+        if (this.debug) {
+            debug(`set rotation: ${value}`, getCaller());
+        }
+    }
+
+    connect(signal, callback) {
+        let handler_id = super.connect(signal, callback);
+
+        if (this.debug) {
+            let caller = getCaller();
+            debug('connect %s (%d)'.format(signal, handler_id), getCaller());
+        }
+
+        return handler_id;
+    }
+
+    disconnect(handler_id) {
+        if (this.debug) {
+            debug('disconnect (%d)'.format(handler_id), getCaller());
+        }
+
+        return super.disconnect(handler_id);
+    }
+}
+);
+
+/**
+ * Implemented the two functions below using tweaked code from:
+ * http://stackoverflow.com/a/13227808
+ */
+
+function getCaller() {
+    let stack = getStack();
+
+    // Remove superfluous function calls on stack
+    stack.shift(); // getCaller --> getStack
+    stack.shift(); // debug --> getCaller
+
+    // Return caller's caller
+    return stack[0];
+}
+
+function getStack() {
+    // Save original Error.prepareStackTrace
+    let origPrepareStackTrace = Error.prepareStackTrace;
+
+    // Override with function that just returns `stack`
+    Error.prepareStackTrace = function (_, stack) {
+        return stack;
+    };
+
+    // Create a new `Error`, which automatically gets `stack`
+    let err = new Error();
+
+    // Evaluate `err.stack`, which calls our new `Error.prepareStackTrace`
+    let stack = err.stack.split("\n");
+
+    // Restore original `Error.prepareStackTrace`
+    Error.prepareStackTrace = origPrepareStackTrace;
+
+    // Remove superfluous function call on stack
+    stack.shift(); // getStack --> Error
+
+    return stack
 }
--- desk-changer@eric.gach.gmail.com/utils.js
+++ desk-changer@eric.gach.gmail.com/utils.js
@@ -1,12 +1,13 @@
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
+const Main = imports.ui.main;
 
 function error(exception, message=null) {
     let output = null;
 
     if (message) {
-        let caller = getCaller();
-        output = '[%s/%s] %s'.format(Me.metadata.uuid, caller.split('/').pop(), message);
+        let caller = Convenience.getCaller();
+        output = `[${Me.metadata.uuid}/${caller.split('/').pop()}] ${message}`;
     }
 
     logError(exception, output);
@@ -16,48 +17,17 @@ function debug(message) {
     let settings = Convenience.getSettings();
 
     if (settings.get_boolean('debug')) {
-        let caller = getCaller();
-        let output = '[' + Me.metadata.uuid + '/' + caller.split('/').pop() + '] ' + message
+        let caller = Convenience.getCaller(),
+            output = `[${Me.metadata.uuid}/${caller.split('/').pop()}] ${message}`;
+
         log(output);
     }
 }
 
-/**
- * Implemented the two functions below using tweaked code from:
- * http://stackoverflow.com/a/13227808
- */
+function notify(message, force) {
+    let settings = Convenience.getSettings();
 
-function getCaller() {
-    let stack = getStack();
-
-    // Remove superfluous function calls on stack
-    stack.shift(); // getCaller --> getStack
-    stack.shift(); // debug --> getCaller
-
-    // Return caller's caller
-    return stack[0];
-}
-
-function getStack() {
-    // Save original Error.prepareStackTrace
-    let origPrepareStackTrace = Error.prepareStackTrace;
-
-    // Override with function that just returns `stack`
-    Error.prepareStackTrace = function (_, stack) {
-        return stack;
-    };
-
-    // Create a new `Error`, which automatically gets `stack`
-    let err = new Error();
-
-    // Evaluate `err.stack`, which calls our new `Error.prepareStackTrace`
-    let stack = err.stack.split("\n");
-
-    // Restore original `Error.prepareStackTrace`
-    Error.prepareStackTrace = origPrepareStackTrace;
-
-    // Remove superfluous function call on stack
-    stack.shift(); // getStack --> Error
-
-    return stack
+    if (settings.notifications || force === true) {
+        Main.notify('DeskChanger', message);
+    }
 }
