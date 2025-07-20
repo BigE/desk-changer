9e4f0f7a071def0b294c49ae25279decd5078154
Eric Gach <eric.gach@gmail.com>
2025-07-20 16:07:32 -0500

    Minor updates

--- po/compile.sh
+++ po/compile.sh
@@ -7,5 +7,4 @@ do
     echo "$l"
     mkdir -p "$DEST/$l/LC_MESSAGES"
     msgfmt "./$l/desk-changer.po" -o "$DEST/$l/LC_MESSAGES/desk-changer.mo"
-    msgfmt "./$l/desk-changer-daemon.po" -o "$DEST/$l/LC_MESSAGES/desk-changer-daemon.mo"
 done
